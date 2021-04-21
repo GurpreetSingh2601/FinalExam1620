b0adc43064d65f0260a4c987a9e80eeb01515428
Gurpreet Singh <gurubahre2019@gmail.com>
2021-04-21 14:35:37 -0700

    Add function getalbumname in js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,3 +4,12 @@ const ALBUMS = [
     { name: "Album 3", price: 9.99, inCart: false },
     { name: "Album 4", price: 19.99, inCart: false }
 ];
+function getAlbumName(name) {
+    for (const album of ALBUMS) {
+        if (album.name == name) {
+            return album;
+        }
+    }
+
+    return "Album name not defined.";
+}
